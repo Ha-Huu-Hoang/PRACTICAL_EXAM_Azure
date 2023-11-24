b552df3d6c95df33d4d6d8e6016aaef32fc4675f
hahuuhoang <hahuuhoang732004@gmail.com>
2023-11-24 15:20:34 +0700

    PRACTICAL_EXAM_Azure

---
+++ exam-azure/Migrations/20231124080330_t2207a.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace exam_azure.Migrations
+{
+    /// <inheritdoc />
+    public partial class t2207a : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderTbl",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemCode = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ItemName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ItemQty = table.Column<int>(type: "int", nullable: false),
+                    OrderDelivery = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OrderAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderTbl", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderTbl");
+        }
+    }
+}
